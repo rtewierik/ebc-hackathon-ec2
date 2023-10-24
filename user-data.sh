1ce942bd27f0c858b369ce0cbccc2f09d89bf881
Ruben te Wierik <ruben.wierik@vista.com>
2023-10-24 18:26:37 +0200

    improve resilience of EC2 instance through launch template and per-boot script.

--- user-data.sh
+++ user-data.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+sudo touch /var/lib/cloud/scripts/per-boot/boot.sh
+echo '#!/bin/bash
+cd /home/ubuntu
 sudo snap install docker
 test -d ebc-hackathon-ec2 || git clone https://github.com/rtewierik/ebc-hackathon-ec2
 cd ebc-hackathon-ec2
-sudo docker-compose up -d
+sudo docker-compose up -d' | sudo tee /var/lib/cloud/scripts/per-boot/boot.sh
+sudo chmod a+rx /var/lib/cloud/scripts/per-boot/boot.sh
