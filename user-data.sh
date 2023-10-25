d9eec9c3ce7f715714c0ff067c1af3710cb6577c
Ruben te Wierik <ruben.wierik@vista.com>
2023-10-25 15:25:15 +0200

    Add registration of new VC types to startup script.

--- user-data.sh
+++ user-data.sh
@@ -5,5 +5,9 @@ cd /home/ubuntu
 sudo snap install docker
 test -d ebc-hackathon-ec2 || git clone https://github.com/rtewierik/ebc-hackathon-ec2
 cd ebc-hackathon-ec2
-sudo docker-compose up -d' | sudo tee /var/lib/cloud/scripts/per-boot/boot.sh
+sudo docker-compose up -d
+sleep 1m
+cd /vc-templates
+curl -d "@VerifiableEvent.json" -X POST localhost:8080/issuer-api/default/config/templates/VerifiableEvent
+curl -d "@VerifiableEmployment.json" -X POST localhost:8080/issuer-api/default/config/templates/VerifiableEmployment' | sudo tee /var/lib/cloud/scripts/per-boot/boot.sh
 sudo chmod a+rx /var/lib/cloud/scripts/per-boot/boot.sh
